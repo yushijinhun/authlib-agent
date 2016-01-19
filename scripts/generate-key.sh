d80eb3554da385014e68f7f0915d4b09003db445
yushijinhun <yushijinhun@gmail.com>
2016-01-19 21:21:36 +0800

    add generate-key.sh

---
+++ scripts/generate-key.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Environment variables:
+# KEY_BITS - the bits of the key
+#
+# Output files:
+# key.pem - the generated rsa key pair
+# publickey.der - the SubjectPublicKeyInfo
+# privatekey.der - the PrivateKeyInfo
+
+# generate keys
+openssl genrsa -out key.pem $KEY_BITS -batch
+openssl req -new -x509 -key key.pem -out cert.pem -batch
+ASN_CERT=`openssl asn1parse -in cert.pem`
+SUB_OFFSET=`echo $ASN_CERT|grep -P -o '\d+(?=:d=\d+\s*hl=\d+\s*l=\s*\d+\s*cons:\s*SEQUENCE\s*\d+:d=\d+\s*hl=\d+\s*l=\s*\d+\s*cons:\s*SEQUENCE\s*\d+:d=\d+\s*hl=\d+\s*l=\s*\d+\s*prim:\s*OBJECT\s*:rsaEncryption)'`
+SUB_LENGTH=`openssl asn1parse -in cert.pem -offset $SUB_OFFSET|grep -P -o '\d+(?=:d=\d+\s*hl=\d+\s*l=\s*\d+\s*cons:\s*cont\s*\[\s*\d+\s*\])'`
+openssl asn1parse -in cert.pem -out publickey.der -offset $SUB_OFFSET -length $SUB_LENGTH -noout
+openssl pkcs8 -in key.pem -out privatekey.der -outform der -topk8 -nocrypt
+
+# cleanup
+rm cert.pem
