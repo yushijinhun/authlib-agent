ab37fbf5320a08b23ae257753685d1118bb98fc8
yushijinhun <yushijinhun@gmail.com>
2016-01-19 11:43:42 +0800

    add InvalidateServlet

---
+++ yggdrasil-backend/yggdrasil-web-impl/src/main/java/yushijinhun/authlibagent/web/servlet/InvalidateServlet.java
@@ -0,0 +1,23 @@
+package yushijinhun.authlibagent.web.servlet;
+
+import static yushijinhun.authlibagent.commons.UUIDUtils.toUUID;
+import java.util.UUID;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import org.json.JSONObject;
+import yushijinhun.authlibagent.web.YggdrasilPostServlet;
+
+@WebServlet("/yggdrasil/invalidate")
+public class InvalidateServlet extends YggdrasilPostServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected JSONObject process(JSONObject req, HttpServletRequest rawReq) throws Exception {
+		UUID clientToken = toUUID(req.getString("clientToken"));
+		UUID accessToken = toUUID(req.getString("accessToken"));
+		backend.invalidate(accessToken, clientToken);
+		return null;
+	}
+
+}
