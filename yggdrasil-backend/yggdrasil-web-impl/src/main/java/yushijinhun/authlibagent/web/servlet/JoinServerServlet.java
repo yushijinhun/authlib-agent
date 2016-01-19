fff612d5cfc65d81e99dc32e8843a019651c6fe2
yushijinhun <yushijinhun@gmail.com>
2016-01-19 12:01:12 +0800

    add JoinServerServlet

---
+++ yggdrasil-backend/yggdrasil-web-impl/src/main/java/yushijinhun/authlibagent/web/servlet/JoinServerServlet.java
@@ -0,0 +1,24 @@
+package yushijinhun.authlibagent.web.servlet;
+
+import java.util.UUID;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import org.json.JSONObject;
+import yushijinhun.authlibagent.web.YggdrasilPostServlet;
+import static yushijinhun.authlibagent.commons.UUIDUtils.toUUID;
+
+@WebServlet("/session/joinserver")
+public class JoinServerServlet extends YggdrasilPostServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected JSONObject process(JSONObject req, HttpServletRequest rawReq) throws Exception {
+		UUID accessToken = toUUID(req.getString("accessToken"));
+		UUID profile = toUUID(req.getString("selectedProfile"));
+		String serverid = req.getString("serverId");
+		backend.joinServer(accessToken, profile, serverid);
+		return null;
+	}
+
+}
