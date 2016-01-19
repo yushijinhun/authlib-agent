a851c5c2a2612771d475c0303986b044e556480d
yushijinhun <yushijinhun@gmail.com>
2016-01-19 17:30:37 +0800

    add GameProfileServlet

---
+++ yggdrasil-backend/yggdrasil-web-impl/src/main/java/yushijinhun/authlibagent/web/servlet/GameProfileServlet.java
@@ -0,0 +1,28 @@
+package yushijinhun.authlibagent.web.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import org.json.JSONObject;
+import yushijinhun.authlibagent.api.web.response.GameProfileResponse;
+import java.util.UUID;
+import static yushijinhun.authlibagent.commons.UUIDUtils.*;
+
+@WebServlet("/session/profiles/minecraft/*")
+public class GameProfileServlet extends YggdrasilGetServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected JSONObject process(HttpServletRequest req) throws Exception {
+		String path = req.getPathTranslated();
+		if (path == null) {
+			return null;
+		}
+
+		UUID profileUUID = toUUID(path);
+		GameProfileResponse profile = backend.lookupProfile(profileUUID);
+
+		return serializer.serializeGameProfile(profile, true);
+	}
+
+}
