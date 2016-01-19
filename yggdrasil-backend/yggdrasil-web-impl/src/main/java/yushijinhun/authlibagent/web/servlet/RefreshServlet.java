b9a80124b1b9360facf161c35d120ba2cfdaed0d
yushijinhun <yushijinhun@gmail.com>
2016-01-19 11:29:11 +0800

    add RefreshServlet

---
+++ yggdrasil-backend/yggdrasil-web-impl/src/main/java/yushijinhun/authlibagent/web/servlet/RefreshServlet.java
@@ -0,0 +1,36 @@
+package yushijinhun.authlibagent.web.servlet;
+
+import java.util.UUID;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import org.json.JSONObject;
+import yushijinhun.authlibagent.api.web.response.AuthenticateResponse;
+import yushijinhun.authlibagent.web.YggdrasilPostServlet;
+import static yushijinhun.authlibagent.commons.UUIDUtils.*;
+
+@WebServlet("/yggdrasil/refresh")
+public class RefreshServlet extends YggdrasilPostServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected JSONObject process(JSONObject req, HttpServletRequest rawReq) throws Exception {
+		UUID clientToken = toUUID(req.getString("clientToken"));
+		UUID accessToken = toUUID(req.getString("accessToken"));
+
+		AuthenticateResponse auth;
+
+		JSONObject newProfileReq = req.optJSONObject("selectedProfile");
+		if (newProfileReq == null) {
+			// refresh
+			auth = backend.refresh(accessToken, clientToken);
+		} else {
+			// select profile
+			UUID newProfile = toUUID(newProfileReq.getString("id"));
+			auth = backend.selectProfile(accessToken, clientToken, newProfile);
+		}
+
+		return serializer.serializeAuthenticateResponse(auth, clientToken);
+	}
+
+}
