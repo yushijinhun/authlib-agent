ef119c760800aeb62687f6ea12c0e029bd8624bb
yushijinhun <yushijinhun@gmail.com>
2016-01-23 22:11:18 +0800

    fix profile path null access

--- yggdrasil-backend/yggdrasil-web-impl/src/main/java/yushijinhun/authlibagent/web/servlet/GameProfileServlet.java
+++ yggdrasil-backend/yggdrasil-web-impl/src/main/java/yushijinhun/authlibagent/web/servlet/GameProfileServlet.java
@@ -14,12 +14,13 @@ public class GameProfileServlet extends YggdrasilGetServlet {
 
 	@Override
 	protected JSONObject process(HttpServletRequest req) throws Exception {
-		String path = req.getPathInfo().substring(1); // delete the / at the beginning
+		String path = req.getPathInfo();
 		if (path == null) {
 			return null;
 		}
 
-		UUID profileUUID = toUUID(path);
+		// substring() - delete the / at the beginning
+		UUID profileUUID = toUUID(path.substring(1));
 		GameProfileResponse profile = backend.lookupProfile(profileUUID);
 
 		return serializer.serializeGameProfile(profile, true);
