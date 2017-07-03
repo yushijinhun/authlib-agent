f2c9a26c9c059b9470d95ecd06dc33883a9937b2
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2017-07-03 13:04:22 +0800

    Transform the class names to JVM internal names in AuthlibClassTransformer
    Origin patch:
    
    From 2161edc0b0bf7673851ca677e4e49f6949d35483 Mon Sep 17 00:00:00 2001
    From: Yanbing Zhao <zzzz@mail.ustc.edu.cn>
    Date: Mon, 3 Jul 2017 12:55:52 +0800
    Subject: [PATCH] Transform the class names to JVM internal names in
     AuthlibClassTransformer
    
    ---
     .../authlibagent/javaagent/tweaker/AuthlibClassTransformer.java
    | 2 +-
     1 file changed, 1 insertion(+), 1 deletion(-)
    
    diff --git
    a/authlib-javaagent/src/main/java/yushijinhun/authlibagent/javaagent/tweaker/AuthlibClassTransformer.java
    b/authlib-javaagent/src/main/java/yushijinhun/authlibagent/javaagent/tweaker/AuthlibClassTransformer.java
    index 92092be..84abee2 100644
    ---
    a/authlib-javaagent/src/main/java/yushijinhun/authlibagent/javaagent/tweaker/AuthlibClassTransformer.java
    +++
    b/authlib-javaagent/src/main/java/yushijinhun/authlibagent/javaagent/tweaker/AuthlibClassTransformer.java
    @@ -19,7 +19,7 @@ public class AuthlibClassTransformer implements
    IClassTransformer {
         public byte[] transform(String name, String transformedName, byte[]
    bytes) {
     		if (name == null || bytes == null) return bytes;
             try {
    -            byte[] result =
                 this.transformer.transform(this.classLoader, name, null,
    null, bytes);
    +            byte[] result =
    this.transformer.transform(this.classLoader, name.replace('.', '/'),
    null, null, bytes);
                 return result == null ? bytes : result;
             } catch (IllegalClassFormatException e) {
                 throw new RuntimeException(e);
    --
    2.11.0

--- authlib-javaagent/src/main/java/yushijinhun/authlibagent/javaagent/tweaker/AuthlibClassTransformer.java
+++ authlib-javaagent/src/main/java/yushijinhun/authlibagent/javaagent/tweaker/AuthlibClassTransformer.java
@@ -19,7 +19,7 @@ public class AuthlibClassTransformer implements IClassTransformer {
     public byte[] transform(String name, String transformedName, byte[] bytes) {
 		if (name == null || bytes == null) return bytes;
         try {
-            byte[] result = this.transformer.transform(this.classLoader, name, null, null, bytes);
+            byte[] result = this.transformer.transform(this.classLoader, name.replace('.', '/'), null, null, bytes);
             return result == null ? bytes : result;
         } catch (IllegalClassFormatException e) {
             throw new RuntimeException(e);
